e2b71a36118280babc9ac41354de3e4d5521ba1b
leoweb32 <leoweb32@gmail.com>
2018-09-19 17:33:58 -0300

    acertos no single

--- single-linha_de_cristais.php
+++ single-linha_de_cristais.php
@@ -20,12 +20,25 @@
 			<img src="<?php echo get_the_post_thumbnail_url() ?>" alt="<?php the_title()?>">
 		</div>
 	</div>
+	<div class="container">
+		<h2>Confira os itens desta linha de produto</h2>
+	</div>
 	<div id="products-itens">
 		<div class="container">
-			<h2>Confira os itens desta linha de produto</h2>
-
-
-
+			<?php // check if the repeater field has rows of data 
+if( have_rows('produtos') ): while ( have_rows('produtos') ) : the_row();  
+$imagem_produto = get_sub_field('foto_do_produto');  
+$nome_produto = get_sub_field('nome_do_produto'); 			
+?>
+			<figure>
+				<a href="<?php echo $imagem_produto['url'] ?>"><img src="<?php echo $imagem_produto['url'] ?>"></a>
+				<figcaption>
+					<?php echo $nome_produto ?> </figcaption>
+			</figure>
+			<? 
+	endwhile; 
+	endif; 
+?>
 		</div>
 	</div>
 </main>
